91f2e83288e12442d850ebea89660a7e73b1c724
RT Syangtan <rnrtamang@gmail.com>
2026-01-04 22:19:44 +0545

    minor changes

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -9,17 +9,24 @@ class HomePage extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final state = ref.watch(Providers.categoryProvider);
     return Scaffold(
+      appBar: AppBar(title: Center(child: Text('Categories')),),
       body: state.when(
           data: (data){
             return GridView.builder(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2),
+                itemCount: data.length,
                 itemBuilder: (context,index){
                   final category = data[index];
-                  return Container(
+                  return Card(
                     child: Column(
                       children: [
-                        Image.network(category.image),
+                        Expanded(
+                          child: ClipRRect(
+                              borderRadius: BorderRadiusGeometry.circular(10),
+                              child: Image.network(category.image,fit: BoxFit.cover,),
+                          ),
+                        ),
                         Text(category.name)
                       ],
                     ),
